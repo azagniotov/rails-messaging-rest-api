bd8bf5802324ddd9d78422a086d59afbba6dff15
Alexander Zagniotov <azagniotov@gmail.com>
2015-10-04 01:01:11 -0500

    Added conversation flow for #index to show all conversations

--- test/integration/conversations_flows_test.rb
+++ test/integration/conversations_flows_test.rb
@@ -13,15 +13,15 @@ class ConversationsFlowsTest < ActionDispatch::IntegrationTest
     credentials = @basic.encode_credentials(@email, @password)
     get '/api/v1/sessions', nil, {'Authorization': credentials}
     @auth_token = response.body
+
+    @user_id = @create_user_json_response['data']['id']
+    post '/api/v1/conversations', {:conversation => {:started_by => @user_id, :message => 'Message', :recipient_ids => [8, 9]}}, {'X-Api-Key': @auth_token}
+    @conversation_json_response = ActiveSupport::JSON.decode response.body
   end
 
   test 'should create new conversation when all params are valid' do
-    user_id = @create_user_json_response['data']['id']
-    post '/api/v1/conversations', {:conversation => {:started_by => user_id, :message => 'Message', :recipient_ids => [8, 9]}}, {'X-Api-Key': @auth_token}
-    conversation_json_response = ActiveSupport::JSON.decode response.body
-
-    assert_equal "#{user_id}", conversation_json_response['data']['attributes']['started_by'].to_s
-    assert_equal '/api/v1/conversations/1', conversation_json_response['data']['attributes']['links']['self']
+    assert_equal "#{@user_id}", @conversation_json_response['data']['attributes']['started_by'].to_s
+    assert_equal '/api/v1/conversations/1', @conversation_json_response['data']['attributes']['links']['self']
   end
 
   test 'should not create new conversation when "started_by" user id does not exist' do
@@ -31,6 +31,14 @@ class ConversationsFlowsTest < ActionDispatch::IntegrationTest
     assert_not_nil error_json_response
     assert_equal "User with id '123456' does not exist", error_json_response['description']
     assert_equal '400', response.code
+  end
+
+  test 'should get all conversations' do
+    get '/api/v1/conversations', nil, {'X-Api-Key': @auth_token}
+    get_json_response = ActiveSupport::JSON.decode response.body
+
+    assert_equal @conversation_json_response['data']['id'], get_json_response['data'][0]['id']
+    assert_equal '200', response.code
   end
 
   def teardown
